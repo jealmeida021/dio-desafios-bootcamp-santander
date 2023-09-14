49e161a5480b91f6398eb18b4e4951b747932fec
jealmeida021 <136184014+jealmeida021@users.noreply.github.com>
2023-09-14 12:08:41 -0300

    Atualização conta terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,23 +1,25 @@
 
-//*Crie um projeto ContaBanco que recebera dados via terminal,
-// contendo as caracterisiticas de conta em banco conforme atributos abaixo :
-//* Dentro do projeto crie a classe ContaTerminal.java para realizar toda a certificação do nosso programa.
-//
-//Atributo     Tipo    Exemplo
-//Numero	     int     1021
-//Agencia      texto    067-8
-//NomeCliente  texto    237.48
+/*Crie um projeto ContaBanco que recebera dados via terminal,
+ contendo as caracterisiticas de conta em banco conforme atributos abaixo :
+ Dentro do projeto crie a classe ContaTerminal.java para realizar toda a certificação do nosso programa.
 
-//*Permita que os dados seja inseridos via terminal sendo que o usario recebera a mensagem de qual
-// informação  sera solicitada
-//
-//> programa "Por Favor digite o numero da agencia";
-//> Usuario 1021 (depois digite o enter)
-//
-//depois de todas as informações terem sidos inserios o sistema dvera exibir a sguinte mensagem :
-//
-//"Ola [Nome Cliente], obrigado por criar uma conta em nosso banco , sua agencia é [Agencia,]
-// conta [Conta] e seu saldo [Saldo], ja sta disponivel para saque.
+Atributo     Tipo    Exemplo
+Numero	     int     1021
+Agencia      texto    067-8
+NomeCliente  texto    237.48
+
+Permita que os dados seja inseridos via terminal sendo que o usario recebera a mensagem de qual
+informação  sera solicitada
+
+ programa "Por Favor digite o numero da agencia";
+Usuario 1021 (depois digite o enter)
+
+depois de todas as informações terem sidos inserios o sistema dvera exibir a sguinte mensagem :
+
+"Ola [Nome Cliente], obrigado por criar uma conta em nosso banco , sua agencia é [Agencia,]
+ conta [Conta] e seu saldo [Saldo], ja sta disponivel para saque.
+
+*/
 
 import java.util.Scanner;
 public class ContaTerminal {
